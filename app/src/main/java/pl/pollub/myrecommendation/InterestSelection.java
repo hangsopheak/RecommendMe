ce85ff8db0a1a3698af8dd34b033e8858e4c4ff9
Sopheak Hang <hangsopheak.hod@gmail.com>
2018-05-09 21:36:41 +0200

    first commit

---
+++ app/src/main/java/pl/pollub/myrecommendation/InterestSelection.java
@@ -0,0 +1,13 @@
+package pl.pollub.myrecommendation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InterestSelection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interest_selection);
+    }
+}
